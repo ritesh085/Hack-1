e49b7d890ecf3cd3d04fa286a3b758d8ad3289ed
Ritesh Mhaske <modeon0975@gmail.com>
2025-11-10 09:08:34 +0530

    removed

--- Backend/middlewares/auth.js
+++
@@ -1,5 +0,0 @@
-// Placeholder auth middleware (expand later)
-module.exports = function (req, res, next) {
-  // Example: validate token if you add JWT
-  next();
-};
--- Backend/routes/hospital.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { listHospitals, createHospital } = require("../controllers/hospitalController");
-
-router.get("/", listHospitals);
-router.post("/", createHospital);
-
-module.exports = router;
